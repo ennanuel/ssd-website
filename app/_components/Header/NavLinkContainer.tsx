80c841babb2ce4e50d29e68430609e14aa4da78a
Emmanuel Ezema <emmanuelzema6@gmail.com>
2025-06-10 16:49:51 +0100

    Added x padding to button

--- app/_components/Header/NavLinkContainer.tsx
+++ app/_components/Header/NavLinkContainer.tsx
@@ -35,7 +35,7 @@ export default function NavLinkContainer({ title, href, subLinks, showingSubLink
                         onClick={toggleSubLinksDisplay} 
                         onMouseOver={displaySubLinks} 
                         onMouseOut={hideSubLinks} 
-                        className="peer lg:group xs:px-6 sm:px-10 lg:px-4 w-full lg:w-fit block text-white lg:hover:text-white/80 border-t lg:border-t-0 border-gray-200/20"
+                        className="peer lg:group px-4 xs:px-6 sm:px-10 lg:px-4 w-full lg:w-fit block text-white lg:hover:text-white/80 border-t lg:border-t-0 border-gray-200/20"
                     >
                         <span className="flex lg:justify-center justify-between items-center gap-6 lg:gap-4 h-14 mx-auto max-w-[var(--max-width)]">
                             <span className="text-sm">{title}</span>
@@ -53,7 +53,7 @@ export default function NavLinkContainer({ title, href, subLinks, showingSubLink
                     <Link 
                         onClick={toggleMobileHeader} 
                         href={href} 
-                        className="lg:group lg:px-4 px-4 xs:px-6 sm:px-10 block text-white hover:text-blue-400 border-t lg:border-t-0 border-gray-200/20"
+                        className="lg:group px-4 xs:px-6 sm:px-10 lg:px-4 block text-white hover:text-blue-400 border-t lg:border-t-0 border-gray-200/20"
                     >
                         <span className="flex items-center justify-between lg:justify-center gap-6 lg:gap-4 h-14 mx-auto max-w-[var(--max-width)]">
                             <span className=" text-sm">{title}</span>
